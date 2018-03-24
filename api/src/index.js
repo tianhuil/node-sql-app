187ca3949f583202d1facb61b6777a074f83cfde
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-03-24 14:31:45 -0400

    Changing message

--- api/src/index.js
+++ api/src/index.js
@@ -44,7 +44,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('API Server Working\n');
 });
 
 app.listen(PORT, HOST);
