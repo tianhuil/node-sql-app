8ccd3b1b3641d18232876184eda6865ca37690cf
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-03-31 11:52:55 -0400

    Breaks up file

--- api/src/index.js
+++ api/src/index.js
@@ -1,41 +1,7 @@
-const Sequelize = require('sequelize');
 const express = require('express');
+const postgres = require('./postgres')
 
-const POSTGRES_USER = process.env.POSTGRES_USER
-      POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD
-      POSTGRES_HOST = process.env.POSTGRES_HOST
-      POSTGRES_DB = process.env.POSTGRES_DB
-      POSTGRES_PORT = process.env.POSTGRES_PORT
-
-console.log(POSTGRES_USER)
-
-const sequelize = new Sequelize(POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, {
-  host: POSTGRES_HOST,
-  port: POSTGRES_PORT,
-  dialect: 'postgres',
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-  // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
-  operatorsAliases: false
-});
-
-const User = sequelize.define('user', {
-  username: Sequelize.STRING,
-  birthday: Sequelize.DATE
-});
-
-sequelize.sync()
-  .then(() => User.create({
-    username: 'janedoe',
-    birthday: new Date(1980, 6, 20)
-  }))
-  .then(jane => {
-    console.log(jane.toJSON());
-  });
+postgres.setup()
 
 // Constants
 const PORT = process.env.API_PORT
@@ -44,7 +10,9 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('API Server Working\n');
+  res.send(JSON.stringify({
+    msg: "Hello World"
+  }));
 });
 
 app.listen(PORT, HOST);
