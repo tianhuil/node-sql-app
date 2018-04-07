e93c72b079c3aa229290b3cfaa629ed1e1d0cda5
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-04-07 00:36:20 -0400

    Fix api CORS

--- api/start.sh
+++ api/start.sh
@@ -1,6 +1,7 @@
 npx postgraphile \
   -n 0.0.0.0 \
   -p $API_PORT \
+  --cors \
   -c postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB \
   --schema forum_example \
   --watch
