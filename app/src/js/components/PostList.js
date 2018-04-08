d3db257b37f1f6685187fd8fd3cf8d5cc58fa408
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-04-08 12:47:39 -0400

    Remove Post.js

--- app/src/js/components/PostList.js
+++ app/src/js/components/PostList.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
-import Post from "./Post";
 
 const postQuery = gql`
 query PostQuery($cursor: Cursor = null) {
@@ -25,6 +24,20 @@ query PostQuery($cursor: Cursor = null) {
   }
 }
 `
+
+const Post = (props) => (
+  <div>
+    <h3>{props.node.headline}</h3>
+    <p>
+      { props.node.personByAuthorId.fullName &&
+          <em className="mr-3">{props.node.personByAuthorId.fullName}</em> }
+      { props.node.topic &&
+          <span className="mr-3">({props.node.topic})</span> }
+      <button type="button" className="btn btn-primary btn-sm">Edit</button>
+    </p>
+    <p>{ props.node.summary }</p>
+  </div>
+)
 
 const PostNextPage = (props) => {
   const pageInfo = props.pageInfo
