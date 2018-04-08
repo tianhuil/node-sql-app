1a8cf005380b8937cb8fc3bd5b04d5e4a45f9d1a
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-04-07 21:14:56 -0400

    Refactoring Query

--- app/src/js/components/box.js
+++ app/src/js/components/box.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
-const query = gql`
-{
-  allPosts(last: 5) {
+const postQuery = gql`
+query PostQuery($cursor: Cursor=null) {
+  allPosts(last: 5, after: $cursor) {
     totalCount
     edges {
       node {
@@ -17,43 +17,45 @@ const query = gql`
         summary(length: 200, omission: "Not Available")
       }
     }
+    pageInfo {
+      endCursor
+      hasNextPage
+    }
   }
 }
 `
 
+const PostList = (props) => (
+  <div className="posts">
+    <h1>Posts ({props.data.allPosts.totalCount})</h1>
+    <ul>
+    {
+      props.data.allPosts.edges.map(e =>
+        <li key={e.node.id}>
+          <h3>{e.node.headline}</h3>
+          <p>
+            { e.node.personByAuthorId.fullName &&
+                <em>{e.node.personByAuthorId.fullName}</em> }
+            &nbsp;&nbsp;
+            { e.node.topic &&
+                <span>({e.node.topic})</span> }
+          </p>
+          <p>{ e.node.summary }</p>
+        </li>
+      )
+    }
+    </ul>
+  </div>
+)
+
 class BookList extends React.Component {
   render() {
     return (
-      <Query query = {query}>
+      <Query query = {postQuery}>
         {({ loading, error, data }) => {
           if (loading) return <p>Loading...</p>;
           if (error) return <p>Error :(</p>;
-
-          return (
-            <div className="posts">
-              <h1>Posts ({data.allPosts.totalCount})</h1>
-              <ul>
-              {
-                data.allPosts.edges.map(e =>
-                  <li key={e.node.id}>
-                    <h3>{e.node.headline}</h3>
-                    <p>
-                      <em>{
-                        e.node.personByAuthorId.fullName ?
-                        e.node.personByAuthorId.fullName : "Anonymous"
-                      }</em>
-                      &nbsp;&nbsp;
-                      ({
-                        e.node.topic ? e.node.topic : "MISC"
-                      })
-                    </p>
-                    <p>{ e.node.summary }</p>
-                  </li>
-                )
-              }
-              </ul>
-            </div>
-          )
+          return <PostList data={data} />
         }}
       </Query>
     );
