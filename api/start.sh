981412a6ca440acf3f7d538311803bb2527e2f2d
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-04-07 21:39:16 -0400

    More error detection in API

--- api/start.sh
+++ api/start.sh
@@ -2,6 +2,8 @@ npx postgraphile \
   -n 0.0.0.0 \
   -p $API_PORT \
   --cors \
+  --show-error-stack \
+  --extended-errors hint,detail,errcode \
   -c postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB \
   --schema forum_example \
   --watch
