6ac4acb81cf2aebb126f836c1525b51696dfa4a8
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-04-07 21:51:18 -0400

    pagination from clicking

--- app/src/js/components/box.js
+++ app/src/js/components/box.js
@@ -3,8 +3,8 @@ import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
 const postQuery = gql`
-query PostQuery($cursor: Cursor=null) {
-  allPosts(last: 5, after: $cursor) {
+query PostQuery($cursor: Cursor = null) {
+  allPosts(first: 5, after: $cursor) {
     totalCount
     edges {
       node {
@@ -25,12 +25,50 @@ query PostQuery($cursor: Cursor=null) {
 }
 `
 
-const PostList = (props) => (
-  <div className="posts">
-    <h1>Posts ({props.data.allPosts.totalCount})</h1>
+const PostList = (props) => {
+  const allPosts = props.allPosts
+  const pageInfo = allPosts.pageInfo
+  const fetchMore = props.fetchMore
+
+  const nextPageClick = (e) => {
+    e.preventDefault();
+    fetchMore({
+      query: postQuery,
+      variables: {
+        cursor: pageInfo.endCursor
+      },
+      updateQuery: (previousResult, { fetchMoreResult }) => {
+        const newEdges = fetchMoreResult.allPosts.edges;
+        const pageInfo = fetchMoreResult.allPosts.pageInfo;
+
+        return newEdges.length
+          ? {
+              allPosts: {
+                totalCount: previousResult.allPosts.totalCount,
+                __typename: previousResult.allPosts.__typename,
+                edges: [...previousResult.allPosts.edges, ...newEdges],
+                pageInfo
+              }
+            }
+          : previousResult;
+      }
+    })
+  }
+
+  const nextPage = (pageInfo.hasNextPage) ? (
+    <button onClick={(e) => nextPageClick(e)} class="btn btn-primary">Next Page</button>
+  ) : (
+    <button onClick={(e) => nextPageClick(e)} class="btn btn-disabled">No More Pages</button>
+  );
+
+  console.log(pageInfo.hasNextPage)
+  console.log(pageInfo.endCursor)
+
+  return <div className="posts">
+    <h1>Posts ({allPosts.totalCount})</h1>
     <ul>
     {
-      props.data.allPosts.edges.map(e =>
+      allPosts.edges.map(e =>
         <li key={e.node.id}>
           <h3>{e.node.headline}</h3>
           <p>
@@ -45,17 +83,19 @@ const PostList = (props) => (
       )
     }
     </ul>
+    { nextPage }
   </div>
-)
+}
 
 class BookList extends React.Component {
   render() {
     return (
-      <Query query = {postQuery}>
-        {({ loading, error, data }) => {
+      <Query query={postQuery}>
+        {({ loading, error, data, fetchMore }) => {
           if (loading) return <p>Loading...</p>;
           if (error) return <p>Error :(</p>;
-          return <PostList data={data} />
+
+          return <PostList allPosts={data.allPosts} fetchMore={fetchMore} />
         }}
       </Query>
     );
