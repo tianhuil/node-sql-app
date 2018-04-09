e1daec39a019c72032c8cf405d6bd0c127b53f80
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-04-09 07:55:13 -0700

    Post Topics Options

--- app/src/js/components/EditPost.js
+++ app/src/js/components/EditPost.js
@@ -17,6 +17,47 @@ query Query($id: Int!) {
 }
 `
 
+const PostTopics = gql`
+{
+  __type(name: "PostTopic") {
+    name
+    enumValues {
+      name
+    }
+    description
+  }
+}
+`
+
+const OptionsWithNull = (props) => (
+  <div className="form-group">
+    <label htmlFor={props.id} className="form-text text-muted">{props.name}</label>
+    <select className="form-control" value={props.select ? props.select : "null"} id={props.id}>
+      {
+        props.options.map(o => <option key={o} value={o}>{o}</option>)
+      }
+      <option key="null" value="null">null</option>
+    </select>
+  </div>
+)
+
+const PostTopicsOptions = (props) => (
+  <Query query={PostTopics}>
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+      console.log(props.select)
+
+      return <OptionsWithNull
+        id="topic"
+        name="Post Topics"
+        select={props.select}
+        options={data.__type.enumValues.map(v => v.name)}
+      />
+    }}
+  </Query>
+)
+
 const EditPost = ({match}) => {
   return <Query query={query} variables={{ id: parseInt(match.params.id) }} >
     {({ loading, error, data }) => {
@@ -24,18 +65,21 @@ const EditPost = ({match}) => {
       if (error) return <p>Error :(</p>;
 
       return <form>
-        <div class="form-group">
-          <label for="headline" className="form-text text-muted">Headline</label>
-          <input type="text" class="form-control" id="headline" aria-describedby="emailHelp" value={data.postById.headline} />
+        <div className="form-group">
+          <label htmlFor="headline" className="form-text text-muted">Headline</label>
+          <input type="text" className="form-control" id="headline" aria-describedby="emailHelp" value={data.postById.headline} />
         </div>
-        <p>
-          <em> {data.postById.personByAuthorId.fullName} </em>
-          {data.postById.topic}
-        </p>
-        <div class="form-group">
-          <label for="headline" className="form-text text-muted">Body</label>
-          <textarea className="form-control" id="body" rows="5">
-            {data.postById.body}
+        <div className="row">
+          <div className="col-6">
+            <em> {data.postById.personByAuthorId.fullName} </em>
+          </div>
+          <div className="col-6">
+            <PostTopicsOptions select={data.postById.topic}/>
+          </div>
+        </div>
+        <div className="form-group">
+          <label htmlFor="headline" className="form-text text-muted">Body</label>
+          <textarea className="form-control" id="body" rows="5" value={data.postById.body}>
           </textarea>
         </div>
       </form>
