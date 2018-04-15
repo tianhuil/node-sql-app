48ce66e5ec97edf01e0f2380c8cb951947c36e75
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-04-15 01:14:56 -0400

    Modularizing EditProfile

--- app/src/js/components/Profile.js
+++ app/src/js/components/Profile.js
@@ -57,23 +57,82 @@ const MutationForm = (props) => (
   </Mutation>
 )
 
-const SmallPostList = (props) => <React.Fragment>
-  <h2 className="mt-4">Posts</h2>
-  <p>Total Posts: {props.totalCount}</p>
-  <ol>
-  {
-    props.nodes.map(node =>
-      <li key={node.id}>
-        <h3>{node.headline}</h3>
-        { node.topic &&
-          <span className="mr-3">({node.topic})</span> }
-        <Link to={`/edit/${node.id}`}>Edit</Link>
-        <p>{ node.summary }</p>
-      </li>
-    )
-  }
-  </ol>
-</React.Fragment>
+const SmallPostList = (props) => (
+  <React.Fragment>
+    <h2 className="mt-4">Posts</h2>
+    <p>Total Posts: {props.totalCount}</p>
+    <ol>
+    {
+      props.nodes.map(node =>
+        <li key={node.id}>
+          <h3>{node.headline}</h3>
+          { node.topic &&
+            <span className="mr-3">({node.topic})</span> }
+          <Link to={`/edit/${node.id}`}>Edit</Link>
+          <p>{ node.summary }</p>
+        </li>
+      )
+    }
+    </ol>
+  </React.Fragment>
+)
+
+const EditProfile = (props) => (
+  <MutationForm
+    mutation={UpdatePerson}
+    handleSubmit={(update, submittedValues) =>
+      update({
+        variables: {
+          input: {
+            id: props.currentPerson.id,
+            personPatch: submittedValues
+          }
+        }
+      })
+    }
+  >
+    { formApi => <React.Fragment>
+      <h2 className="mt-4">Author Information</h2>
+      <form onSubmit={formApi.submitForm}>
+        <div className="row">
+          <div className="col-xs-12 col-md-4 form-group">
+            <label htmlFor="firstName">First Name</label>
+            <Text className="form-control"
+              id="firstName" field="firstName"
+              defaultValue={props.currentPerson.firstName}
+            />
+          </div>
+          <div className="col-xs-12 col-md-4 form-group">
+            <label htmlFor="lastName">First Name</label>
+            <Text className="form-control"
+              id="lastName" field="lastName"
+              defaultValue={props.currentPerson.lastName}
+            />
+          </div>
+          <div className="col-xs-12 col-md-4">
+            <label htmlFor="createdAt">First Name</label>
+            <input className="form-control"
+              id="createdAt"
+              value={props.currentPerson.createdAt}
+              disabled
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <label htmlFor="lastName">About</label>
+          <TextArea
+            className="form-control" rows="5"
+            id="about" field="about"
+            defaultValue={props.currentPerson.about}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Save
+        </button>
+      </form>
+    </React.Fragment>}
+  </MutationForm>
+)
 
 const Profile = (props) => (
   <Query query={QueryPerson}>
@@ -82,60 +141,9 @@ const Profile = (props) => (
       if (error) return <p>Error :(</p>
 
       return <React.Fragment>
-        <MutationForm
-          mutation={UpdatePerson}
-          handleSubmit={(update, submittedValues) =>
-            update({
-              variables: {
-                input: {
-                  id: data.currentPerson.id,
-                  personPatch: submittedValues
-                }
-              }
-            })
-          }
-        >
-          { formApi => <React.Fragment>
-            <h2 className="mt-4">Author Information</h2>
-            <form onSubmit={formApi.submitForm}>
-              <div className="row">
-                <div className="col-xs-12 col-md-4 form-group">
-                  <label htmlFor="firstName">First Name</label>
-                  <Text className="form-control"
-                    id="firstName" field="firstName"
-                    defaultValue={data.currentPerson.firstName}
-                  />
-                </div>
-                <div className="col-xs-12 col-md-4 form-group">
-                  <label htmlFor="lastName">First Name</label>
-                  <Text className="form-control"
-                    id="lastName" field="lastName"
-                    defaultValue={data.currentPerson.lastName}
-                  />
-                </div>
-                <div className="col-xs-12 col-md-4">
-                  <label htmlFor="createdAt">First Name</label>
-                  <input className="form-control"
-                    id="createdAt"
-                    value={data.currentPerson.createdAt}
-                    disabled
-                  />
-                </div>
-              </div>
-              <div className="form-group">
-                <label htmlFor="lastName">About</label>
-                <TextArea
-                  className="form-control" rows="5"
-                  id="about" field="about"
-                  defaultValue={data.currentPerson.about}
-                />
-              </div>
-              <button type="submit" className="btn btn-primary">
-                Save
-              </button>
-            </form>
-          </React.Fragment>}
-        </MutationForm>
+        <EditProfile
+          currentPerson={data.currentPerson}
+        />
         <SmallPostList
           nodes={data.currentPerson.postsByAuthorId.nodes}
           totalCount={data.currentPerson.postsByAuthorId.totalCount}
